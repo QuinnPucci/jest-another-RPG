a985e21d4d4b16ebd12729fabd4fb350688b22ee
QuinnPooch <pucciquinn@gmail.com>
2021-12-26 17:06:15 -0500

    used similar player methods and tests to write 4 more methods and corresponding test for enemy values

--- __tests__/Enemy.test.js
+++ __tests__/Enemy.test.js
@@ -14,3 +14,44 @@ test('creates an enemy object', () => {
     expect(enemy.agility).toEqual(expect.any(Number))
     expect(enemy.potion).toEqual(expect.any(Object))
 })  
+
+// test for enemy health value method
+test("gets enemy's health value", () => {
+    const enemy = new Enemy('goblin', 'sword');
+  
+    expect(enemy.getHealth()).toEqual(expect.stringContaining(enemy.health.toString()));
+  })
+
+// test for enemy alive method
+test('checks if enemy is alive or not', () => {
+    const enemy = new Enemy('goblin', 'sword');
+  
+    expect(enemy.isAlive()).toBeTruthy();
+  
+    enemy.health = 0;
+  
+    expect(enemy.isAlive()).toBeFalsy();
+})
+
+// test for enemy attack value method
+test("gets enemy's attack value", () => {
+    const enemy = new Enemy('goblin', 'sword');
+    enemy.strength = 10;
+  
+    expect(enemy.getAttackValue()).toBeGreaterThanOrEqual(5);
+    expect(enemy.getAttackValue()).toBeLessThanOrEqual(15);
+})
+
+// test for lowering health method
+test("subtracts from enemy's health", () => {
+    const enemy = new Enemy('goblin', 'sword');
+    const oldHealth = enemy.health;
+  
+    enemy.reduceHealth(5);
+  
+    expect(enemy.health).toBe(oldHealth - 5);
+  
+    enemy.reduceHealth(99999);
+  
+    expect(enemy.health).toBe(0);
+})
